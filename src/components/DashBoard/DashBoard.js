8be3832bd21760eea392e8abb37a60cb67b0f0b6
abdurrazzakhera <hhera1413@gmail.com>
2022-04-04 15:54:27 +0600

    rechar add and netlify relode 404 issu solve

--- src/components/DashBoard/DashBoard.js
+++ src/components/DashBoard/DashBoard.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { LineChart } from "recharts";
 import "./DashBoard.css";
 
 const DashBoard = () => {
   return (
     <div>
       <h1>DashBoard</h1>
+      <div>
+        <LineChart></LineChart>
+      </div>
     </div>
   );
 };
