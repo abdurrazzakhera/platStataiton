94c55c8d018cea829905129ed1bcf289dd0fc354
abdurrazzakhera <hhera1413@gmail.com>
2022-04-04 16:13:26 +0600

    chart added

--- src/components/DashBoard/DashBoard.js
+++ src/components/DashBoard/DashBoard.js
@@ -1,13 +1,44 @@
-import React from "react";
-import { LineChart } from "recharts";
+import React, { useEffect, useState } from "react";
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 import "./DashBoard.css";
 
 const DashBoard = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("chartData.json")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
   return (
     <div>
       <h1>DashBoard</h1>
       <div>
-        <LineChart></LineChart>
+        <LineChart data={data} width={600} height={500}>
+          <Line type='monotone' dataKey='revenue' stroke='#8884d8'></Line>
+          <CartesianGrid stroke='#ccc' />
+          <XAxis dataKey='investment' />
+          <YAxis />
+          <Tooltip></Tooltip>
+        </LineChart>
+        <BarChart width={730} height={250} data={data}>
+          <CartesianGrid strokeDasharray='3 3' />
+          <XAxis dataKey='month' />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey='investment' fill='#8884d8' />
+          <Bar dataKey='revenue' fill='#82ca9d' />
+        </BarChart>
       </div>
     </div>
   );
