24abe79f36b386b47ed3efc377b84cf0db988448
abdurrazzakhera <hhera1413@gmail.com>
2022-04-05 16:18:37 +0600

    not found page add

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className='container text-center'>
+      <div className='mt-10'>
+        <h2 className='text-xl'>
+          The Page Not Found !!!!
+          <br />
+          <strong className='text-5xl font-bold text-sky-600'>404</strong>
+        </h2>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
